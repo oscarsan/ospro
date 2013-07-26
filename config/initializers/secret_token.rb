b2d03c33f128cedd21dbdd0190db4c7b3faae626
oscarsan <oscarsanhueza@gmail.com>
2013-07-26 21:04:03 +0300

    First commint, just create application in rails Signed-off-by: oscarsan <oscarsanhueza@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ospro::Application.config.secret_token = 'bcd8b2f5c9bb583c771bdc35f7dca9b3d229a0cd6cf816ecb35cf69445d27ad26cbc29657a15ec9c39df7c4b622bf5c98ae7d07b8071bc0306b2ba2630d2d599'
