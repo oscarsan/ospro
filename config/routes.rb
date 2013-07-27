daa0eb95002b0b8d2529cb7d4d01e405ab3f327d
oscarsan <oscarsanhueza@gmail.com>
2013-07-27 17:14:08 +0300

    Add users controller
    Signed-off-by: oscarsan <oscarsanhueza@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,8 @@ Ospro::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => 'welcome#index'
+   
+   resource :users
 
   # See how all your routes lay out with "rake routes"
 
